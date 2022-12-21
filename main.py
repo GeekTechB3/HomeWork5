77d800ab953405e9124e723690eb102e986453e5
denchikaa <denizovchyngyz93@gmail.com>
2022-12-21 18:53:04 +0300

    first

---
+++ main.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+def application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+
+    window.setWindowTitle("GeekTech")
+    window.setGeometry(500,500,500,500)
+
+    main_text = QtWidgets.QLabel(window)
+    main_text.setText("GeekCoin 150 KGS")
+
+    window.show()
+    sys.exit(app.exec_())
+
+application()
