f138a2036afe5b0bc1de243e608f8dd226d3ea3c
denchikaa <denizovchyngyz93@gmail.com>
2022-12-28 15:41:56 +0300

    first

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
 
+from PyQt5 import  QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys 
+
+def add_button_lable():
+    print("Hello world")
 
 def application():
     app = QApplication(sys.argv)
     window = QMainWindow()
 
-    window.setWindowTitle("GeekTech")
-    window.setGeometry(500,500,500,500)
+    window.setWindowTitle("Простая программа")
+    window.setGeometry(300, 250, 500, 500)
 
     main_text = QtWidgets.QLabel(window)
-    main_text.setText("GeekCoin 150 KGS")
+    main_text.setText("Hello World ")
+    main_text.move(100,0)
+
+    button = QtWidgets.QPushButton(window)
+    button.setText("Нажми на меня")
+    button.clicked.connect(add_button_lable)
 
     window.show()
     sys.exit(app.exec_())
+    
 
-application()
+if __name__ == "__main__":
+    application()                                                
